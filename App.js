4a54bcaae9bf42bae229b9d0e4f4685463a7a38b
DanDevelop22 <dan22.develop@gmail.com>
2022-12-16 13:30:06 -0500

    Terminando configuracion nativeWind

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
+
+
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 
 const App = () => {
 
   return(
-    <View className="flex items-center bg-red-700">
-      <Text>Hola</Text>
+    <View className="flex-1 items-center justify-center m-10">
+      <Text className="text-2xl my-2">Me cago en ti</Text>
+      <Pressable className="bg-blue-500 p-3 rounded-lg" onPress={() => {alert("Me cago en ti")}}>
+        <Text className="text-white">Tocame</Text>
+      </Pressable>
+      
     </View>
   )
 }
